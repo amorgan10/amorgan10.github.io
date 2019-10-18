1aca6766e914af88e8b41091224597500d7949f0
amorgan10 <55769941+amorgan10@users.noreply.github.com>
2019-10-17 17:14:13 -0700

    fixed folder names

--- processing/first_project/sketch.js
+++ processing/first_project/sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-  // put setup code here
+  CanvasGradient(800, 600);
   }
   function draw() {
-  background(204);
-  ellipse(50, 50, 80, 80);
+  if(mousepressed) {
+    fill (0); 
   }
+  else {
+    fill (255)
+  } 
+  
+ellipse(mouseX, mouseY, 80, 80)
+}
