5c688aff3818c02246c3ce4e23df39203ae5ac30
AJ <amorgan10@cocc.edu>
2019-10-20 00:59:13 -0700

    added JS projects to home page

--- processing/second_project/sketch.js
+++ processing/second_project/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  // put setup code here
+  createCanvas(1366, 768)
 }
 
 function draw() {
-  // put drawing code here
+if (mouseIsPressed) {  
+  ellipse (683, 384, 400, 100, 30, 0, 360, false);
+  ellipse (683, 384, 300, 50, 30, 0, 360, false);
+  ellipse (900, 400, 10, 10);
+  ellipse (300, 200, 10, 10);
+  ellipse (800, 500, 10, 10);
+  ellipse (683, 384, 200, 200);
+}
+  ellipse (683, 384, 200, 200)
 }
