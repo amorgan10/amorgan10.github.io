f0d5f91023e83024d14950f4417d02ac6ab291f7
AJ <amorgan10@cocc.edu>
2019-12-05 15:53:09 -0800

    Added 3rd JS drawing

--- processing/third_project/sketch.js
+++ processing/third_project/sketch.js
@@ -1,14 +1,23 @@
 function setup() {
-  createCanvas(250, 250)
+  createCanvas(500, 500)
 }
 
-function draw() {{
+let value = 0;
+function draw() {
+  fill(value);
+  ellipse(200, 200, 300, 300);
+}
+function keyPressed() {
+  if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
+}
   if (mouseIsPressed){
     c = color(random(0, 255), random(0, 255), random(0, 255));
     fill (c);
+    rect(250, 250, 300, 300)
   }
-  
-}
-ellipse(125,125,125,125)
-}
+
 
